14649286e4a3b25bd22263df8e1abfd31ab76619
jiauzhang <1743960454@qq.com>
2019-12-26 22:25:08 +0800

    add crosstool-ng dependencies

---
+++ tools/install-crosstool-dep.sh
@@ -0,0 +1,10 @@
+# autoreconf flex unzip help2man
+sudo apt-get install -y autoconf flex unzip help2man
+# makeinfo
+sudo apt-get install -y texinfo
+# libtool
+sudo apt-get install -y libtool libtool-bin
+# curses
+sudo apt-get install libncurses-dev
+# yacc
+sudo apt-get install flex biso
