cd453fe9409d79897562c8d1b8fa7c94602e3003
jiauzhang <1743960454@qq.com>
2019-12-31 21:15:11 +0800

    add i386 dependencies for wsl

---
+++ scripts/install-docker.sh
@@ -0,0 +1,16 @@
+# sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get install apt-transport-https ca-certificates curl \
+	gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+	sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+# verify that docker engine
+# sudo docker run hello-world
+# uninstall docker engine
+# sudo apt-get purge docker-ce
+# sudo rm -rf /var/lib/docker
