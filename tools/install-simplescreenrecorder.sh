5372566ba11dbdf4633ab6eda5bb13687b598532
jiauzhang <1743960454@qq.com>
2020-01-02 22:39:55 +0800

    add screen recorder tools

---
+++ tools/install-simplescreenrecorder.sh
@@ -0,0 +1,10 @@
+# add source
+sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
+# update source
+sudo apt-get update
+# install simplescreenrecorder to record screen
+sudo apt-get install simplescreenrecorder
+# install screenkey to display your keys
+sudo apt-get install screenkey
+# install fish shell
+# sudo apt-get install fish
